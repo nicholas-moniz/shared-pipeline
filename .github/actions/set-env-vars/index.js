38e69dac89c3fdc5fcc2ef43e3951291c3ac7b70
Nicholas Moniz <99140906+NicoM-7@users.noreply.github.com>
2025-05-02 15:59:49 -0400

    Update index.js

--- .github/actions/set-env-vars/index.js
+++ .github/actions/set-env-vars/index.js
@@ -5,15 +5,10 @@ const workspace = process.env.GITHUB_WORKSPACE;
 const envFile = path.join(workspace, "runtime.env");
 
 const entries = [
-  { key: "PATH", value: `${workspace}/tools/bin:${process.env.PATH}` },
-  { key: "CALLER_PATH", value: `${workspace}/${process.env.GITHUB_REPOSITORY.split("/")[-1]}` },
-  { key: "LIBRARY_PATH", value: `${workspace}/shared-pipeline` },
-  { key: "ACTIONS_PATH", value: `${workspace}/shared-pipeline/.github/actions` },
-  { key: "SCRIPTS_PATH", value: `${workspace}/shared-pipeline/scripts`}
+  { key: "PATH", value: `${workspace}/tools/bin:${process.env.PATH}` }
 ];
 
-// Write to runtime.env
-fs.writeFileSync(envFile, '');
+fs.writeFileSync(envFile, "");
 entries.forEach(({ key, value }) => {
   fs.appendFileSync(envFile, `${key}=${value}\n`);
 });
