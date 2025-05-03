9a0a54087892686a2b8c7e07326c7bcf23e47b03
Nicholas Moniz <99140906+NicoM-7@users.noreply.github.com>
2025-05-03 02:21:11 -0400

    Create node.js

---
+++ .github/actions/validate-build-properties/schemas/node.js
@@ -0,0 +1,46 @@
+const schema = z.object({ 
+  project: z.object({
+    name: z.string(),
+    version: z.string().regex(/^\d+\.\d+\.\d+$/, {
+      message: "Version must be in x.x.x format",
+    }),
+    registry: z.string().default("ghcr.io/nicholas-moniz"),
+    test: z
+      .object({
+        config: z.string().default("jest.config.js"),
+        directory: z.string().default("."),
+        args: z.string().default(""),
+      })
+      .partial()
+      .default({}),
+    docker: z
+      .object({
+        dockerfile: z.string().default("dockerfile"),
+        context: z.string().default("."),
+        buildArgs: z.record(z.string()).default({}),
+        buildCommandArgs: z.string().default(""),
+      })
+      .partial()
+      .default({}),
+    helm: z
+      .object({
+        chartPath: z.string().default("helm"),
+        namespace: z.string().default("default"),
+      })
+      .partial()
+      .default({}),
+  }),
+  overrides: z
+    .object({
+      releaseBranch: z.boolean().default(false),
+      skipUnitTests: z.boolean().default(false),
+      autoIncrementVersion: z.boolean().default(true),
+      publishDockerImage: z.boolean().default(true),
+      publishHelmChart: z.boolean().default(true),
+      deployToCluster: z.boolean().default(true),
+    })
+    .partial()
+    .default({}),
+});
+
+module.exports = { schema };
