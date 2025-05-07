e2a96e9f9e914bbe36d1018a51e5d20a8c587273
Nicholas Moniz <99140906+NicoM-7@users.noreply.github.com>
2025-05-07 16:56:22 -0400

    Update octokit.js

--- scripts/octokit.js
+++ scripts/octokit.js
@@ -1,28 +1,9 @@
-const { Octokit } = require("@octokit/rest");
-const core = require("@actions/core");
+const github = require("@actions/github");
 
-async function main() {
-  try {
-    const octokit = new Octokit({ auth: process.env.NICHOLAS_MONIZ_GITHUB_TOKEN });
-
-    const owner = process.env.GITHUB_REPOSITORY.split('/')[0];
-    const repo = process.env.GITHUB_REPOSITORY.split('/')[1];
-    const runId = process.env.GITHUB_RUN_ID;
-
-    const run = await octokit.request('GET /repos/{owner}/{repo}/actions/runs/{run_id}', {
-      owner,
-      repo,
-      run_id: runId
-    });
-
-    const runName = run.data.name;
-    core.info(`Run name: ${runName}`);
-
-    const buildPath = runName.replace(/^build-/, "").trim();
-    core.setOutput("build-properties-path", buildPath);
-  } catch (err) {
-    core.setFailed(`❌ ${err.message}`);
-  }
+if (!process.env.NICHOLAS_MONIZ_GITHUB_TOKEN) {
+  throw new Error("GitHub token not found in environment variable NICHOLAS_MONIZ_GITHUB_TOKEN");
 }
 
-main();
+const octokit = github.getOctokit(process.env.NICHOLAS_MONIZ_GITHUB_TOKEN);
+
+module.exports = octokit;
