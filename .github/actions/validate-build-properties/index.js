d05d0618f5df28718991a5f36640ee9f162aebbe
Nicholas Moniz <99140906+NicoM-7@users.noreply.github.com>
2025-05-08 18:28:09 -0400

    Update index.js

--- .github/actions/validate-build-properties/index.js
+++ .github/actions/validate-build-properties/index.js
@@ -1,29 +1,26 @@
-const fs = require("fs");
-const path = require("path");
-const core = require("@actions/core");
 const { node } = require("./schemas/index");
 const { ZodError } = require("zod");
 
-module.exports = function (build) {
+module.exports = function ({ fs, path, core, env }) {
   try {
     let schema;
-    switch(process.env.BUILD_TYPE) {
+    switch(env.BUILD_TYPE) {
       case "NODE":
         schema = node;
         break;
       default:
-        throw new Error(`Invalid build type ${process.env.BUILD_TYPE}`);
+        throw new Error(`Invalid build type ${env.BUILD_TYPE}`);
     }
     
-    const parsed = schema.parse(JSON.parse(process.env.BUILD_PROPERTIES_PATH));
+    const parsed = schema.parse(JSON.parse(env.BUILD_PROPERTIES_PATH));
     fs.writeFileSync(process.env.BUILD_PATH, JSON.stringify(parsed, null, 2));
-    core.info(`${process.env.BUILD_PROPERTIES_PATH} has been validated and parsed at ${process.env.BUILD_PATH}`);
+    core.info(`${process.env.BUILD_PROPERTIES_PATH} has been validated and parsed at ${env.BUILD_PATH}`);
   } catch (err) {
     if (err instanceof ZodError) {
-      core.error(`Failed to validate ${process.env.BUILD_PROPERTIES_PATH} for the following reasons`);
+      core.error(`Failed to validate ${env.BUILD_PROPERTIES_PATH} for the following reasons`);
       err.errors.forEach(e => core.error(JSON.stringify(e, null, 2)));
     } else {
-      core.error(`An unexpected error occured while trying to validate ${process.env.BUILD_PROPERTIES_PATH} for the following reason: ${err.message}`);
+      core.error(`An unexpected error occured while trying to validate ${env.BUILD_PROPERTIES_PATH} for the following reason: ${err.message}`);
     }
 
     throw new Error("An error occured while trying to validate build properties");
