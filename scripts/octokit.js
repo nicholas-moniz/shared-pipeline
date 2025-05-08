092c70f35012e28a58f1b0978e868f855f786cf9
Nicholas Moniz <99140906+NicoM-7@users.noreply.github.com>
2025-05-08 17:15:56 -0400

    Delete scripts/octokit.js

--- scripts/octokit.js
+++
@@ -1,9 +0,0 @@
-const github = require("@actions/github");
-
-if (!process.env.NICHOLAS_MONIZ_GITHUB_TOKEN) {
-  throw new Error("GitHub token not found in environment variable NICHOLAS_MONIZ_GITHUB_TOKEN");
-}
-
-const octokit = github.getOctokit(process.env.NICHOLAS_MONIZ_GITHUB_TOKEN);
-
-module.exports = octokit;
