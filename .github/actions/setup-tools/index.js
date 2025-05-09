dba66cdf28811518bc675a60560c1c26b062dcd9
Nicholas Moniz <99140906+NicoM-7@users.noreply.github.com>
2025-05-09 17:48:00 -0400

    Update index.js

--- .github/actions/setup-tools/index.js
+++ .github/actions/setup-tools/index.js
@@ -1,6 +1,6 @@
 module.exports = async function ({ core, env, exec, fs, path }) {
   try {
-    await fs.mkdir(env.TOOLS_DIR);
+    await fs.mkdir(env.TOOLS_DIR, { recursive: true });
     
     const tools = JSON.parse(await fs.readFile(path.join(__dirname, "tools.json"), "utf-8"));
     const installScriptPath = path.join(__dirname, "install.sh");
