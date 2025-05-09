c7fff44b2d39c66f5a43d3f6fed91695568cbb6c
Nicholas Moniz <99140906+NicoM-7@users.noreply.github.com>
2025-05-09 14:29:49 -0400

    Update index.js

--- .github/actions/set-env-vars/index.js
+++ .github/actions/set-env-vars/index.js
@@ -2,12 +2,13 @@ module.exports = async function ({ env, core, fs, path }) {
   try {
     const workspace = env.GITHUB_WORKSPACE;
     const envFile = path.join(workspace, "runtime.env");
+    const repoName = env.GITHUB_REPOSITORY.split("/").pop();
 
     const entries = [
       { key: "PATH", value: `${workspace}/tools/bin:${env.PATH}` },
-      { key: "CALLER_PATH", value: `${workspace}/${env.GITHUB_REPOSITORY.split("/").pop()}` },
-      { key: "LIBRARY_PATH", value: `${workspace}/${env.GITHUB_REPOSITORY.split("/").pop()}/shared-pipeline` },
-      { key: "BUILD_PROPERTIES_PATH", value: `${workspace}/${env.GITHUB_REPOSITORY.split("/").pop()}/${env.BUILD_PROPERTIES_PATH}` },
+      { key: "CALLER_PATH", value: `${workspace}/${repoName}` },
+      { key: "LIBRARY_PATH", value: `${workspace}/shared-pipeline` },
+      { key: "BUILD_PROPERTIES_PATH", value: `${workspace}/${repoName}/${env.BUILD_PROPERTIES_PATH}` },
       { key: "LIBRARY_VERSION", value: env.LIBRARY_VERSION },
       { key: "WORKFLOW", value: env.WORKFLOW },
     ];
