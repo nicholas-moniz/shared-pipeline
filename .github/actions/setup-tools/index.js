9b19ce5f054583066d0d879dfd81fa4450d0da98
Nicholas Moniz <99140906+NicoM-7@users.noreply.github.com>
2025-05-09 17:46:31 -0400

    Update index.js

--- .github/actions/setup-tools/index.js
+++ .github/actions/setup-tools/index.js
@@ -1,18 +1,14 @@
 module.exports = async function ({ core, env, exec, fs, path }) {
-  const installScriptPath = path.join(__dirname, "install.sh");
-
-  async function installTool(tool) {
-    const { name, url, method, bashArgs = "" } = tool;
-    await exec.exec("bash", [installScriptPath, method, name, url, bashArgs]);
-  }
-
   try {
     await fs.mkdir(env.TOOLS_DIR);
+    
     const tools = JSON.parse(await fs.readFile(path.join(__dirname, "tools.json"), "utf-8"));
+    const installScriptPath = path.join(__dirname, "install.sh");
 
     for (const tool of tools) {
       core.info(`Installing ${tool.name}`);
-      await installTool(tool);
+      const { name, url, method, bashArgs = "" } = tool;
+      await exec.exec("bash", [installScriptPath, method, name, url, bashArgs]);
     }
   } catch (err) {
     core.error(`Tool installation failed: ${err}`);
